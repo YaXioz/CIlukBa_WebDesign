ed0f31813d426e09e2bf7481db2545244ac7b5ff
andhika777 <andhikahendriansyah@gmail.com>
2024-09-26 13:12:12 +0700

    singin

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -3,7 +3,7 @@ import SigninForm from "../ui/signin-form";
 
 export default function Page() {
   return (
-    <div className="container flex">
+    <div className="container">
       <SigninForm></SigninForm>
     </div>
   );
--- src/app/ui/signin-form.jsx
+++ src/app/ui/signin-form.jsx
@@ -7,21 +7,32 @@ export default function SigninForm() {
   const [state, action] = useFormState(login, undefined);
 
   return (
+    <div className="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 min-h-screen">
+      <div className="relative container m-auto px-6 md:text-black text-gray-500 md:px-12 xl:px-40"> 
+        <div className="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
+        <div className="rounded-xl bg-white shadow-xl">
+          <div className="p-6 sm:p-16">
+            
     <form action={action}>
-      <div>
+      <div className="grid mb-6">
         <label htmlFor="email">Email</label>
-        <input id="email" name="email" placeholder="john@example.com" />
+        <input className="bg-gray-200 p-2 text-black rounded-lg" id="email" name="email" placeholder="john@example.com" />
       </div>
       {state?.errors?.email && <p>{state.errors.email}</p>}
 
-      <div>
+      <div className="grid mb-6">
         <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
+        <input className="bg-gray-200 text-black p-2 rounded-lg" id="password" name="password" type="password" placeholder="enter your password" />
       </div>
       {state?.errors?.password && <p>{state.errors.password}</p>}
 
       <SubmitButton />
     </form>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
   );
 }
 
@@ -29,8 +40,10 @@ function SubmitButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button disabled={pending} type="submit">
-      Sign Up
-    </button>
+    <div className="flex justify-center">
+      <button className="bg-gray-800 text-white p-2 rounded-lg" disabled={pending} type="submit">
+        Sign Up
+      </button>
+    </div>
   );
 }
