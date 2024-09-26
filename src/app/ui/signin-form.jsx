f5151ad9d3ea9246832c11e42537854206a0adbf
brogangin <terompettrumpet@gmail.com>
2024-09-26 15:11:05 +0700

    fix diganti alfian

--- src/app/ui/signin-form.jsx
+++ src/app/ui/signin-form.jsx
@@ -9,34 +9,42 @@ export default function SigninForm() {
 
   return (
     <div className="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 min-h-screen">
-      <div className="relative container m-auto px-6 md:text-black text-gray-500 md:px-12 xl:px-40"> 
-      <div class="flex justify-center mb-4"> <img src="/image/Logo Black.png" alt="Logo" class="h-12"/> </div> 
+      <div className="relative container m-auto px-6 md:text-black text-gray-500 md:px-12 xl:px-40">
+        <div class="flex justify-center mb-4">
+          {" "}
+          <img src="/image/Logo Black.png" alt="Logo" class="h-12" />{" "}
+        </div>
         <div className="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
-        <div className="rounded-xl bg-[#1a1a1a] shadow-xl">
-          <div className="p-6 sm:p-16">
-            
-    <form action={action}>
-      <div className="grid mb-6">
-        <label htmlFor="email" label className="mb-4 text-white">Email</label>
-        <input className="bg-gray-200 p-2 text-black rounded-lg" id="email" name="email" placeholder="john@example.com" />
-      </div>
-      {state?.errors?.email && <p>{state.errors.email}</p>}
+          <div className="rounded-xl bg-[#1a1a1a] shadow-xl">
+            <div className="p-6 sm:p-16">
+              <form action={action}>
+                <div className="grid mb-6">
+                  <label htmlFor="email" label className="mb-4 text-white">
+                    Email
+                  </label>
+                  <input className="bg-gray-200 p-2 text-black rounded-lg" id="email" name="email" placeholder="john@example.com" />
+                </div>
+                {state?.errors?.email && <p>{state.errors.email}</p>}
 
-      <div className="grid mb-6">
-        <label htmlFor="password" label className="mb-4 text-white">Password</label>
-        <input className="bg-gray-200 text-black p-2 rounded-lg" id="password" name="password" type="password" placeholder="Enter your password" />
-      </div>
-      {state?.errors?.password && <p>{state.errors.password}</p>}
+                <div className="grid mb-6">
+                  <label htmlFor="password" label className="mb-4 text-white">
+                    Password
+                  </label>
+                  <input className="bg-gray-200 text-black p-2 rounded-lg" id="password" name="password" type="password" placeholder="Enter your password" />
+                </div>
+                {state?.errors?.password && <p>{state.errors.password}</p>}
 
-      <SubmitButton />
-    </form>
-    <div className="mt-5">
-      <Link className="text-white" href="signin">Don't have an account?</Link>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
+                <SubmitButton />
+              </form>
+              <div className="mt-5">
+                <Link className="text-white" href="signup">
+                  Don&apos;t have an account?
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -47,9 +55,10 @@ function SubmitButton() {
   return (
     <div className="flex justify-center">
       <div class="b mx-auto h-16 w-64 flex justify-center items-center">
-        <div class="i h-16 w-64 bg-gradient-to-br from-blue-400 to-blue-900 items-center rounded-full shadow-2xl cursor-pointer absolute overflow-hidden transform hover:scale-x-110 hover:scale-y-105 transition duration-300 ease-out">
-        </div>
-        <a class="text-center text-white font-semibold z-10 pointer-events-none">Sign In!</a>
+        <div class="i h-16 w-64 bg-gradient-to-br from-blue-400 to-blue-900 items-center rounded-full shadow-2xl cursor-pointer absolute overflow-hidden transform hover:scale-x-110 hover:scale-y-105 transition duration-300 ease-out"></div>
+        <button className="text-center text-white font-semibold z-10 pointer-events-none" disabled={pending} type="submit">
+          Sign In!
+        </button>
       </div>
     </div>
   );
--- src/app/ui/signup-form.jsx
+++ src/app/ui/signup-form.jsx
@@ -10,7 +10,10 @@ export default function SignupForm() {
   return (
     <div className="relative py-16  min-h-screen">
       <div className="relative container m-auto px-6 md:text-black text-gray-500 md:px-12 xl:px-40">
-      <div class="flex justify-center mb-4"> <img src="/image/Logo Black.png" alt="Logo" class="h-12"/> </div> 
+        <div class="flex justify-center mb-4">
+          {" "}
+          <img src="/image/Logo Black.png" alt="Logo" class="h-12" />{" "}
+        </div>
         <div className="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
           <div className="rounded-xl bg-[#1a1a1a]">
             <div className="p-6 sm:p-16">
@@ -57,7 +60,9 @@ export default function SignupForm() {
                 <SubmitButton />
               </form>
               <div className="mt-5">
-                <Link className="text-white" href="signin">Already have an account?</Link>
+                <Link className="text-white" href="signin">
+                  Already have an account?
+                </Link>
               </div>
             </div>
           </div>
@@ -73,9 +78,10 @@ function SubmitButton() {
   return (
     <div className="flex justify-center">
       <div class="b mx-auto h-16 w-64 flex justify-center items-center">
-        <div class="i h-16 w-64 bg-gradient-to-br from-blue-400 to-blue-900 items-center rounded-full shadow-2xl cursor-pointer absolute overflow-hidden transform hover:scale-x-110 hover:scale-y-105 transition duration-300 ease-out">
-        </div>
-        <a class="text-center text-white font-semibold z-10 pointer-events-none">Create Account!</a>
+        <div class="i h-16 w-64 bg-gradient-to-br from-blue-400 to-blue-900 items-center rounded-full shadow-2xl cursor-pointer absolute overflow-hidden transform hover:scale-x-110 hover:scale-y-105 transition duration-300 ease-out"></div>
+        <button className="text-center text-white font-semibold z-10 pointer-events-none" disabled={pending} type="submit">
+          Create Account!
+        </button>
       </div>
     </div>
   );
