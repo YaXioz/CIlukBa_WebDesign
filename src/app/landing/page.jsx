1c0444c4a93216425cf1ea24cfbcddd8b4a6590d
Alfianzyh <alfianzyh@gmail.com>
2024-09-30 18:06:40 +0700

    edit posts 2015

---
+++ src/app/landing/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div className="w-screen h-screen bg-[#171d22]">
+        <div class="flex justify-center pt-12 mb-12">
+          {" "}
+          <img src="/image/logo-white.png" alt="Logo" class="h-14" />{" "}
+        </div>
+        <div class="w-[712.90px] h-screen absolute top-0 left-[-100] bg-gradient-to-r from-[#23B4DB] to-[#311996] rounded-full "></div>
+    </div>
+  );
+}
