c305eb0c77d4e155464d7f8f41ec8030267d7045
Alfianzyh <alfianzyh@gmail.com>
2024-10-12 02:57:30 +0700

    delete components

--- src/app/components/button.jsx
+++
@@ -1,5 +0,0 @@
-const Button = (props: React.PropsWithChildren) => {
-    return (
-        <button className="border py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a]">Start Making Journey</button>
-    )
-}
